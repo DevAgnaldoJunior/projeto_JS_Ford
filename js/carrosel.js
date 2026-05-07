c68807b9e3cdcc5d56ce344e7cee29e1f5c3007f
DevAgnaldoJunior <junioragnaldo335@gmail.com>
2026-05-07 16:34:25 -0300

    add: iniciando carrosel

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,32 +1,41 @@
-
-
-//carousel
-
-//Array storage class
 let carouselArr = [];
 
-
-//class Carousel
 class Carousel {
+    constructor(image, title, link) {
+        this.image = image;
+        this.title = title;
+        this.link = link;
+    }
 
-    
-      
-    static Start(arr){
-        if(arr){
+    static Start(arr) {
+        Carousel._arr = arr;
+        Carousel._sequence = 0;
+        Carousel._size = arr.length;
 
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
+        Carousel.Next();
+
+        setInterval(() => {
+            Carousel.Next();
+        }, 5000);
+    }
+
+    static Next() {
+        const item = Carousel._arr[Carousel._sequence];
+
+        console.log("Imagem inserida:", item.image);
+        
+        document.getElementById("carousel").innerHTML = `
+            <a href="${item.link}">
+                <img src="${item.image}" alt="${item.title}" style="width: 500px;">
+            </a>
+        `;
+
+        document.getElementById("carousel-title").innerHTML = item.title;
+
+        Carousel._sequence++;
+
+        if (Carousel._sequence >= Carousel._size) {
+            Carousel._sequence = 0;
         }
     }
-
-    static Next(){
-        
-    }
-};
+}
